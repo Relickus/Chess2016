07bd0445b9573262e02b12868d7b48ee3717f65f
vojta <vojta@vojta-ThinkPadT520>
2016-06-01 00:26:59 +0200

    Created printable MoveList, printPossibleMoves() method works. Fixed memory leaks due to bad menu deallocs.

--- main.cpp
+++ main.cpp
@@ -14,15 +14,23 @@ int main(int argc, char** argv) {
     CController * c = new CController();
 
     c->showMenus();
-    c->startGame();
     
-    CKing * k = dynamic_cast<CKing*>(c->getGameSess().gameBoard.getPiece(0,4));
+    c->getGameSess().start();
     
-    cout<<endl << k->getName();
-    CBoard & b =c->getGameSess().gameBoard;
+    MoveList l = c->getGameSess().gameBoard.getPiece(0,1)->getLegalMoves(c->getGameSess().gameBoard);
     
-    cout << k->isChecked(b);
+    l.print();
     
+    c->getGameSess().gameBoard.printPossibleMoves(l);
+//    
+//    CKing * k = dynamic_cast<CKing*>(c->getGameSess().gameBoard.getPiece(0,4));
+//    
+//    cout<<endl << k->getName();
+//    CBoard & b =c->getGameSess().gameBoard;
+//    
+//    cout << k->isChecked(b);
+//    
+    c->endGame();
     delete c;
     return 0;
 }
